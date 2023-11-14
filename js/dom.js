013621378dbd028a4d3f9dcb5f00702a1709aea8
fantom030983 <ihorkavun1234@gmail.com>
2023-11-14 23:39:47 +0200

    ClassWork_dom

---
+++ js/dom.js
@@ -0,0 +1,28 @@
+// let div = document.createElement('div');
+// div.append("Hello World");
+
+// div.innerHTML = "Hello World2"
+
+// document.body.appendChild(div); //вивід на сторінку
+
+// let h1 = document.createElement('h1');
+// h1.innerHTML = "Classwork JS"
+
+// let p = document.createElement('p');
+// p.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos incidunt voluptatem placeat ex odio officiis quidem necessitatibus unde molestiae consequatur distinctio et dolore sequi delectus assumenda facere, quia harum excepturi pariatur. Dicta labore asperiores a quam corporis obcaecati vel blanditiis cum, iste quod quae aliquam odio distinctio dolorem mollitia, atque nihil optio fuga debitis pariatur suscipit deserunt. Ullam error quod neque veniam inventore, fugit vel id, ad quisquam quibusdam tempora, possimus sit unde doloremque aspernatur tenetur quas totam dignissimos voluptatibus expedita. Numquam, praesentium! Libero harum iusto omnis molestias earum. Autem hic dolor illum itaque pariatur excepturi dignissimos ab provident distinctio?"
+
+// console.log(div)
+
+// let element = document.createElement ("тег");//Создание элемента
+
+// div.id = "my-div"; или // div.className = "someClass"; //додавання класів або ID
+
+// div.innerHTML = "Some text with <b>bold text</b>"; //додавання тексту
+// или
+// let text = document.createTextNode("Some text");
+// div.appendChild(text);
+
+
+
+
+  
